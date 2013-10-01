c8b96d27adb65c19b6e8e137f42c3f5450827923
PRo5 <pro5@PRo5s-iMac.local>
2013-10-01 09:48:02 +0200

    first commit

--- tempGit/AppDelegate.m
+++ tempGit/AppDelegate.m
@@ -45,6 +45,7 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
+    NSLog(@"sdf");
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
